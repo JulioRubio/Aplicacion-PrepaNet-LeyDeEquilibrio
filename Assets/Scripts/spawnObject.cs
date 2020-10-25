3a2b93baa859af62debb97def7ae45396bddea36
JulioRubio <julioarg97@gmail.com>
2020-10-25 14:26:51 -0600

    Fixed issues, added fixed joint preventing object movement on platform
    Commit anterior tenia fallas, cambios no guardados.
    
    Se agrego fixed joint, falta solucionar un problema cuando se crea el objeto, todos los objetos deberan aparecer en posicion y 0.7 y la posicion x del spawner seleccionado.

---
+++ Assets/Scripts/spawnObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FixedJoint2D fixedJoint2D = GetComponent<FixedJoint2D>();
+        fixedJoint2D.enabled = true;
+
+        Vector2 newPos = new Vector2(Mathf.Round(fixedJoint2D.connectedAnchor.x), 0.7F);
+        fixedJoint2D.connectedAnchor = newPos;
+        print(fixedJoint2D.connectedAnchor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
