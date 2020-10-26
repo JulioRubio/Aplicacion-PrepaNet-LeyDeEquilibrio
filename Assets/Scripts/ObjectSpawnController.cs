e3b05cf9949bcb761cd595299efd23585265a09b
JulioRubio <julioarg97@gmail.com>
2020-10-26 09:42:07 -0600

    assets faltantes en commit anterior
    El boton por alguna razon no se incluyo en el commit anterior, se vuelve a subir.
    
    Se soluciono salto que daban objetos cuando se creaban a traves del spawner

--- Assets/Scripts/ObjectSpawnController.cs
+++ Assets/Scripts/ObjectSpawnController.cs
@@ -15,11 +15,11 @@ public class ObjectSpawnController : MonoBehaviour
             Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
             Vector2 touchPos = new Vector2(wp.x, wp.y);
             if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos) && Input.GetTouch(0).phase == TouchPhase.Began) {
-
-                var newObj = GameObject.Instantiate(spawn, touchPos, Quaternion.identity);
+                Destroy(originalSprite);
+                Vector2 newPos = new Vector2(originalSprite.transform.position.x,originalSprite.transform.position.y);
+                var newObj = GameObject.Instantiate(spawn, newPos, Quaternion.identity);
                 newObj.AddComponent<spawnObject>();
                 newObj.transform.parent = GameObject.Find("Platform").transform;
-                Destroy(originalSprite);
             }
         }
     }
