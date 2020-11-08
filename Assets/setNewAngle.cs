81ea88dc2085f4262abfe685b2169f8c56fb409a
Miguel <miguel971029@gmail.com>
2020-11-07 21:59:07 -0600

    Textadded
    Aded text to objects

---
+++ Assets/setNewAngle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setNewAngle : MonoBehaviour
+{
+    public Transform actualTransform;
+    public Transform newTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        actualTransform.eulerAngles = newTransform.eulerAngles;
+    }
+}
