03d6bc92fa88b0f0f35820f136dcf1cfcb48a601
psalazartec <43385170+psalazartec@users.noreply.github.com>
2020-11-10 10:02:00 -0600

    MenuPrincipal
    Se agregó menú principal con botones cuales llevan a SampleScene. Falta estética.

---
+++ Assets/Scripts/CambiaEscenas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CambiaEscenas : MonoBehaviour
+{
+	public void ChangeScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+		if (scene == "Salir")
+			Application.Quit();
+	}
+}
