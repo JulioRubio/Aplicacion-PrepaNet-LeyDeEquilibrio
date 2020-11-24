b4026ac6605fc8ede2fe4359aea0ab43216f79ec
psalazartec <43385170+psalazartec@users.noreply.github.com>
2020-11-24 00:39:10 -0600

    Cambio a Reiniciar
    Se agrego script reiniciar en Tutorial

---
+++ Assets/Scripts/Reiniciar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Reiniciar : MonoBehaviour
+{
+    
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
